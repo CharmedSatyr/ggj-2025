d869c0f94fc473a04291832adf42d86659c7ffbc
Joseph Wolfe <joseph@WLF.to>
2025-01-26 17:17:01 -0800

    Changes at the end

--- Assets/Scripts/Player/Controller.cs
+++ Assets/Scripts/Player/Controller.cs
@@ -43,14 +43,43 @@ namespace Player
 
         void Win()
         {
-            StartCoroutine(Wait());
-
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+            StartCoroutine(ReturnToTitleAfterWait());
         }
 
-        IEnumerator Wait()
+        IEnumerator ReturnToTitleAfterWait()
         {
-            yield return new WaitForSeconds(10f);
+            float fadeDuration = 10f;
+
+            AudioSource[] audioSources = FindObjectsByType<AudioSource>(FindObjectsSortMode.None);
+
+            float[] initialVolumes = new float[audioSources.Length];
+            for (int i = 0; i < audioSources.Length; i++)
+            {
+                initialVolumes[i] = audioSources[i].volume;
+            }
+
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fadeDuration)
+            {
+                elapsedTime += Time.deltaTime;
+
+                float t = elapsedTime / fadeDuration;
+
+                for (int i = 0; i < audioSources.Length; i++)
+                {
+                    if (audioSources[i] != null)
+                    {
+                        audioSources[i].volume = Mathf.Lerp(initialVolumes[i], 0f, t);
+                    }
+                }
+
+                yield return null;
+            }
+
+            yield return new WaitForSeconds(fadeDuration);
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
         }
 
         IEnumerator ResetWithDelay(int seconds)
@@ -61,6 +90,5 @@ namespace Player
 
             GetComponent<SpriteRenderer>().enabled = true;
         }
-
     }
 }
